d48799adb2eca6590dae1da8cd558e5f29c31e04
Dariusz <lavxo@wp.pl>
2021-10-27 19:36:07 +0200

    Initial commit, add base configuration files.

---
+++ src/main/java/pl/wolkowski/summonerfetcher/RestSummonerFetcherApplication.java
@@ -0,0 +1,13 @@
+package pl.wolkowski.summonerfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSummonerFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSummonerFetcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wolkowski/summonerfetcher/RestRiotFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wolkowski.summonerfetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestRiotFetcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
